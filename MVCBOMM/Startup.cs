d85bb67d4f7445f7c2364a34d5e727574e040643
OmerMir1 <omermir907@gmail.com>
2018-01-05 13:07:43 +0530

    Add project files.

---
+++ MVCBOMM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCBOMM.Startup))]
+namespace MVCBOMM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
